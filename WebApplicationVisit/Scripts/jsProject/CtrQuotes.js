501633e5eb7a256e1e65450870a35ab3edf1b3ee
Laura Marcela Grisales García <lmgrisales@unipanamericana.edu.co>
2016-10-30 22:37:02 -0500

    Cotización1
    Se modifició el CtrQuotes.js para poder ver los formularios
    correctamente

--- WebApplicationVisit/Scripts/jsProject/CtrQuotes.js
+++ WebApplicationVisit/Scripts/jsProject/CtrQuotes.js
@@ -4,12 +4,16 @@ Create :DIEGO CASALLAS
 Date :26/10/2016
 
 */
+//Declarate Variable 
+var sNameTitle = "Cotización"
+//Start DOM html jQuery
 $(document).ready(function () {
 
 
     $('.button-collapse').sideNav();
     selectionItem(0);
     $('.modal-trigger').leanModal();
+    $('select').material_select();
   //validateSession(document.URL);
 
 });
@@ -21,21 +25,33 @@ function selectionItem(item) {
         $(items+i).css('color', 'black');
     }
     $(items + item).css('color', '#29b6f6');
-    selectionSubItems(item + "_0");
+    selectionSubItems(item + "_0", " ", 0);
   
 }
 //Function select sub Items
-function selectionSubItems(subItem) {
+function selectionSubItems(subItem, Form, typeSelection) {
     let items = "#subItem_" + subItem;
- 
+
     $('.collection-item').css("background-color", "#fff");
     $('.collection-item').css("color", "#26a69a");
 
     $(items).css("background-color", "#26a69a");
     $(items).css("color", "#eafaf9");
-   
+
     let text = $(items).text();
-    $('#titleForm').text(text);
+    $('#titleForm').text(sNameTitle + " " + text);
+
+
+    if (Form != " ") {
+        subItem = Form;
+    }
+    if (typeSelection != 0) {
+        $('#cont_search_' + subItem).css("display", "block");
+        disableEnableInput(subItem, 0)
+    } else {
+        $('#cont_search_' + subItem).css("display", "none");
+        disableEnableInput(subItem, 1)
+    }
     selectForm(subItem);
 
 }
@@ -74,7 +90,9 @@ function selectionMsubItems(subItem,item) {
 //Function select form  
 function selectForm(dataForm) {
     $(".form_select").css("display", "none");
-    $('#form' + dataForm).fadeIn("slow");
+    let form = '#form' + dataForm;
+    $(form).fadeIn("slow");
+    clearInput(form);
 }
 //Function validate create
 //Load data of form
